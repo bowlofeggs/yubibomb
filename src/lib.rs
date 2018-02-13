e647b3223810113d7e2722ee660812681d3026ae
Randy Barlow <randy@electronsweatshop.com>
2018-02-13 16:14:57 -0500

    Document the purpose of the yubibomb lib.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+//! # yubibomb
+//!
+//! ```yubibomb``` returns fake HOTP tokens so you can paste them into IRC.
 
 extern crate rand;
 
